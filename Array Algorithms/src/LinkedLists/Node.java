84c78d7fe51765228dd4ac892fd838e47fb6aa37
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-07-18 19:32:07 +0100

    [ADD] DoublyLinkedList created

--- Array Algorithms/src/LinkedLists/Node.java
+++ Array Algorithms/src/LinkedLists/Node.java
@@ -4,10 +4,12 @@ public class Node {
 
     public int data;
     public Node next;
+    public Node prev;
 
     public Node (int data) {
         this.data = data;
         this.next = null;
+        this.prev = null;
     }
 
 }
--- Array Algorithms/src/Main.java
+++ Array Algorithms/src/Main.java
@@ -3,7 +3,7 @@ import LinkedLists.Node;
 public class Main {
 
     public static void main(String[] args) {
-        createLinkedList();
+        createDoublyLinkedList();
     }
 
     public static void createLinkedList() {
@@ -23,6 +23,40 @@ public class Main {
             currentNode = currentNode.next;
         }
         System.out.println("null");
+
+    }
+
+    public static void createDoublyLinkedList() {
+
+        Node node1 = new Node(3);
+        Node node2 = new Node(5);
+        node1.next = node2;
+        node2.prev = node1;
+
+        Node node3 = new Node(13);
+        node2.next = node3;
+        node3.prev = node2;
+
+        Node node4 = new Node(2);
+        node3.next = node4;
+        node4.prev = node3;
+
+        Node currentNode = node1;
+        System.out.print("Forward: ");
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
+
+        currentNode = node4;
+        System.out.print("Backward: ");
+        while (currentNode != null) {
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.prev;
+        }
+        System.out.println("null");
+
 
     }
 
