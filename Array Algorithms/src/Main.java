4fa540f5cf7ce2d0e83b6dc9028d3e91694d9102
JamesSinnott1994 <JamesRichardSinnott@gmail.com>
2024-07-19 12:35:52 +0100

    [ADD] Traversal of a linked list

--- Array Algorithms/src/Main.java
+++ Array Algorithms/src/Main.java
@@ -3,7 +3,8 @@ import LinkedLists.Node;
 public class Main {
 
     public static void main(String[] args) {
-        createDoublyLinkedList();
+        //createDoublyLinkedList();
+        traverseAndPrintRun();
     }
 
     public static void createLinkedList() {
@@ -82,6 +83,30 @@ public class Main {
         }
 
         System.out.println("...");  // Indicating the list loops back
+    }
+
+    public static void traverseAndPrintRun() {
+        Node node1 = new Node(7);
+        Node node2 = new Node(11);
+        Node node3 = new Node(3);
+        Node node4 = new Node(2);
+        Node node5 = new Node(9);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        traverseAndPrint(node1);
+    }
+
+    public static void traverseAndPrint(Node head) {
+        Node currentNode = head;
+        while(currentNode != null) {
+            System.out.print(currentNode.data + " -> ");
+            currentNode = currentNode.next;
+        }
+        System.out.println("null");
     }
 
 }
